6f8701eb58796a653d832662999867de7a6d5543
Кірюша <mkiril087@gmail.com>
2025-04-05 08:29:13 +0300

    gh

--- index1.js
+++ index1.js
@@ -1,10 +1,18 @@
 const btnPress = document.querySelector(`.press_money`);
 const moneyFoot = document.querySelector(`.money_footer`);
 const sorry = document.querySelector(`.sorry`);
-
+const hb = document.querySelector(`.hb`);
+const container = document.querySelector(`.container`);
+const btnHb = document.querySelector(`.btnHb`);
 
 btnPress.addEventListener(`click`, (e) => {
     e.preventDefault();
     moneyFoot.style.display = `block`;
     sorry.style.display = `block`;
+})
+
+hb.addEventListener(`click`, (e) => {
+    e.preventDefault();
+    container.style.display = `block`;
+    btnHb.style.display = `none`;
 })
